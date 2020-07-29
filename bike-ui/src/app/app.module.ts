e48e8b3a52f10825696b8db58c566791e7626fea
Markus Suzuki <markus_suzuki@hotmail.com>
2020-07-29 07:00:29 -0400

    Finished Angular, without server.js

--- bike-ui/src/app/app.module.ts
+++ bike-ui/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 import { BikeService } from './services/bike.service';
-import { HttpClientModule } from '@angular/common/http';
+
 import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    HomeComponent,
+    ViewRegistrationComponent,
   ],
   imports: [
     BrowserModule, 
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [BikeService],
   bootstrap: [AppComponent]
